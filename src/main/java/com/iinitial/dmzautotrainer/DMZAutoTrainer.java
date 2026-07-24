5aaaf482417c598d7e9ff9b522b6712fea1855d5
iinitial <noah.robinsg@gmail.com>
2026-07-24 14:20:00 -0400

    Added mod version variable

--- src/main/java/com/iinitial/dmzautotrainer/DMZAutoTrainer.java
+++ src/main/java/com/iinitial/dmzautotrainer/DMZAutoTrainer.java
@@ -4,16 +4,17 @@ import com.iinitial.dmzautotrainer.common.config.ConfigManager;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.loading.FMLLoader;
 
 @Mod(DMZAutoTrainer.MOD_ID)
 public class DMZAutoTrainer {
     public static final String MOD_ID = "dmzautotrainer";
+    private static final String MOD_VERSION = FMLLoader.getLoadingModList().getModFileById(MOD_ID).versionString();
 
     public DMZAutoTrainer() {
         ModLoadingContext.get().registerConfig(
                 ModConfig.Type.CLIENT,
                 ConfigManager.SPEC
         );
-
     }
 }
