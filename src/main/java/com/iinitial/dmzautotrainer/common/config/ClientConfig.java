2e9f2ecc6185e45957302877d180f6ca4e0f9f5c
iinitial <noah.robinsg@gmail.com>
2026-07-24 18:46:59 -0400

    Added getters and setters for client configs

--- src/main/java/com/iinitial/dmzautotrainer/common/config/ClientConfig.java
+++ src/main/java/com/iinitial/dmzautotrainer/common/config/ClientConfig.java
@@ -1,7 +1,29 @@
 package com.iinitial.dmzautotrainer.common.config;
 
+import java.io.ObjectInputFilter;
+
 public final class ClientConfig {
-    public boolean enableAutoTrainer = false;
-    public boolean enableRepeatTraining = true;
-    public int     levelsToComplete = 50;
+    public static boolean enableAutoTrain = false;
+    public static boolean enableRepeatTraining = true;
+    public static float   levelsToComplete = 50;
+
+    public static boolean getAutoTrain() { return enableAutoTrain; }
+
+    public static boolean getRepeatTraining() { return enableRepeatTraining; }
+
+    public static float getLevelsToComplete() { return levelsToComplete; }
+
+
+    public static void setAutoTrain(boolean autoTrain) {
+        enableAutoTrain = autoTrain;
+        ConfigManager.saveClientConfig();
+    }
+
+    public static void setRepeatTraining(boolean repeatTraining) {
+        enableRepeatTraining = repeatTraining;
+    }
+
+    public static void setLevelsToComplete(float levels) {
+        levelsToComplete = levels;
+    }
 }
