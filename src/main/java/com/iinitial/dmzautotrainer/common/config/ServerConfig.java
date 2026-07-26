a122e715fc4155f1683b4468dd0bd8c7a835d5f0
iinitial <noah.robinsg@gmail.com>
2026-07-26 19:51:08 -0400

    removed pointless comment as its purpose is stated in README

--- src/main/java/com/iinitial/dmzautotrainer/common/config/ServerConfig.java
+++ src/main/java/com/iinitial/dmzautotrainer/common/config/ServerConfig.java
@@ -2,7 +2,6 @@ package com.iinitial.dmzautotrainer.common.config;
 
 public class ServerConfig {
     public boolean enableSessions = false;
-    // durations in seconds
     public int sessionDuration = 900;
     public int sessionCooldown = 900;
 
