bda647e0a01e2c49b65ad0ae8a7fa5f87faed487
Kiziro <K.Akami@outlook.com>
2026-08-07 19:52:29 +0200

    feat(config): add enableAutoTrainer master gate to the server config (#494)

--- src/main/java/com/iinitial/dmzautotrainer/common/config/ServerConfig.java
+++ src/main/java/com/iinitial/dmzautotrainer/common/config/ServerConfig.java
@@ -1,9 +1,12 @@
 package com.iinitial.dmzautotrainer.common.config;
 
 public class ServerConfig {
+    public boolean enableAutoTrainer = true;
     public boolean enableSessions = false;
     public int sessionDuration = 900;
     public int sessionCooldown = 900;
+
+    public boolean isAutoTrainerEnabled() { return enableAutoTrainer; }
 
     public boolean isSessionsEnabled() { return enableSessions; }
 
